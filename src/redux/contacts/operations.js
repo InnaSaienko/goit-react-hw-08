f49a8413ef02e327e9af31ad4432610592f40f54
Inna Saienko <inna.saienko.nl@gmail.com>
2025-05-13 09:35:41 +0200

    created a separate Axios instance

--- src/redux/contacts/operations.js
+++ src/redux/contacts/operations.js
@@ -1,13 +1,15 @@
 import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-axios.defaults.baseURL = "https://680a5972d5075a76d987b767.mockapi.io";
+export const goitAPI = axios.create({
+    baseURL: 'https://680a5972d5075a76d987b767.mockapi.io',
+});
 
 export const fetchContacts = createAsyncThunk(
     "contacts/fetchAll",
     async (_, thunkAPI) => {
         try {
-            const response = await axios.get("/contacts");
+            const response = await goitAPI.get("/contacts");
             return response.data;
         } catch (e) {
             return thunkAPI.rejectWithValue(e.message);
@@ -18,7 +20,7 @@ export const addContact = createAsyncThunk(
     "contacts/addContact",
     async (newContact, thunkAPI) => {
         try {
-            const response = await axios.post("/contacts", newContact);
+            const response = await goitAPI.post("/contacts", newContact);
             return response.data;
         } catch (e) {
             return thunkAPI.rejectWithValue(e.message);
@@ -29,7 +31,7 @@ export const deleteContact = createAsyncThunk(
     "contacts/deleteContact",
     async (id, thunkAPI) => {
         try {
-            await axios.delete(`/contacts/${id}`);
+            await goitAPI.delete(`/contacts/${id}`);
             return id;
         } catch (e) {
             return thunkAPI.rejectWithValue(e.message);
